2e477402b4d0b3b0ee7aa57513a8bc2172deeb57
study657 <79162199605@list.ru>
2022-05-30 23:13:25 +0300

    Полностью готовое приложение, с вылавливанием всех возможных ошибок

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import steam.webauth as wa
 import datetime
 import requests
+import smtplib
 import json
 import time
 import re
+import os
 
 current_time = str(datetime.datetime.today().strftime(f"%Y-%m-%d %H:%M:%S"))
-work_time = str(datetime.datetime(2022, 6, 12, 0, 0, 0))
+work_time = str(datetime.datetime(2022, 6, 30, 0, 0, 0))
+mistake_pass = 0
+mistake_global = 0
+
+with open('config.json') as json_file:
+    dataJSON = json.load(json_file)
+    items_for_search = dataJSON['items_for_search']
 
 
 if current_time < work_time:
@@ -66,25 +74,77 @@ if current_time < work_time:
 
 
     def start_script(GAME, items_for_search):
+        global mistake_pass
         current_account_num = 1
         accounts_with_drop = []
         accounts_without_drop = []
         accounts_with_empty_inventory = []
+        accounts_with_mistake_pass = []
+
+        def getAllStatistic(accounts_with_mistake_pass, accounts_without_drop, accounts_with_empty_inventory, accounts_with_drop, folder=''):
+            if len(accounts_with_mistake_pass) != 0:
+                with open(folder + 'accounts_with_mistake_pass.txt', "w") as file:
+                    file.writelines("%s\n" % line for line in accounts_with_mistake_pass)
+
+            if len(accounts_without_drop) != 0:
+                with open(folder + 'accounts_without_drop.txt', "w") as file:
+                    file.writelines("%s\n" % line for line in accounts_without_drop)
+
+            if len(accounts_with_empty_inventory) != 0:
+                with open(folder + 'accounts_with_empty_inventory.txt', "w") as file:
+                    file.writelines("%s\n" % line for line in accounts_with_empty_inventory)
+
+            with open(folder + 'Accounts_with_drop.txt', "w") as file:
+                file.writelines("%s\n" % line for line in accounts_with_drop)
+
+            with open(folder + 'Statistic.txt', "w") as file:
+                file.writelines('Общее кол-во кейсов: ' + sumCases(items_for_search) + '\n\n')
+                file.writelines('Кол-во кейсов каждого вида:\n\n')
+                for item in items_for_search:
+                    file.writelines(item['name_item'] + ': ' + str(item['quantity']) + '\n')
 
         with open("Accounts.txt", "r") as file:
             for line in file:
                 while True:
-                    loginAndPass = re.split(':', line.strip(), maxsplit=0)
-                    print(loginAndPass[0], current_account_num)
-                    
-                    user = wa.WebAuth(loginAndPass[0])
-                    session = user.cli_login(loginAndPass[1])
+                    try:
+                        loginAndPass = re.split(':', line.strip(), maxsplit=0)
+                        print(loginAndPass[0], current_account_num)
+                        
+                        user = wa.WebAuth(loginAndPass[0])
+                        try:
+                            user.login(loginAndPass[1])
+                            mistake_pass = 0
+                        except (wa.CaptchaRequired, wa.LoginIncorrect) as exp:
+                            if isinstance(exp, wa.LoginIncorrect):
+                                if mistake_pass < 2:
+                                    print('Неверный пароль. Засыпаю на 120 секунд!')
+                                    mistake_pass = mistake_pass + 1
+                                    time.sleep(120)
+                                    continue
+                                else:
+                                    accounts_with_mistake_pass.append(loginAndPass[0] + ':' + loginAndPass[1])
+                                    mistake_pass = 0
+                                    current_account_num = current_account_num + 1
+                                    print('С аккаунтом ' + loginAndPass[1] + ' какие-то проблемы, поэтому я его пропускаю!')
+                                    time.sleep(10)
+                                    break
 
-                    response = requests.get('https://steamcommunity.com/profiles/' + str(user.steam_id) + '/inventory/json/' + INVENTORY[GAME] + '/2', cookies=user.session.cookies, headers = {'User-agent': 'your bot 0.1'})
+                            if isinstance(exp, wa.CaptchaRequired):
+                                print('Steam начал требовать captcha, поэтому засыпаю на 10 минут')
+                                print(user.captcha_url)
+                                time.sleep(600)
+                                continue
+                                # ask a human to solve captcha
+                            # else:
+                            #     captcha = None
 
-                    if response.status_code == 200:   
-                        if response.json()['success']:
-                            try:
+                            # user.login(loginAndPass[1], captcha=captcha)
+
+                        response = requests.get('https://steamcommunity.com/profiles/' + str(user.steam_id) + '/inventory/json/' + INVENTORY[GAME] + '/2', cookies=user.session.cookies, headers = {'User-agent': 'your bot 0.1'})
+
+                        if response.status_code == 200: 
+                            if response.json()['success']:
+                                # try:
                                 rgInventory_fromJSON = response.json()['rgInventory']
 
                                 if len(rgInventory_fromJSON) != 0:
@@ -103,57 +163,77 @@ if current_time < work_time:
                                     accounts_without_drop.append(loginAndPass[0] + ':' + loginAndPass[1])
                                     print('У аккаунта ' + loginAndPass[0] + ' инвентарь пуст!')
                                     break
-                            except:
-                                print('У аккаунта ' + loginAndPass[0] + ' непредвиденная ошибка!')
-                                time.sleep(25)
+                                # except Exception as ex:
+                                #     template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                                #     message = template.format(type(ex).__name__, ex.args)
+                                #     print('У аккаунта ' + loginAndPass[0] + ' непредвиденная ошибка! ' + message)
+                                #     time.sleep(25)
+                            else:
+                                current_account_num = current_account_num + 1
+                                accounts_with_empty_inventory.append(loginAndPass[0] + ':' + loginAndPass[1])
+                                print('У аккаунта ' + loginAndPass[0] + ' не создан инвентарь в Вашей игре!')
+                                break
                         else:
+                            print('Ошибка ', response.status_code + '. Засыпаю на 180 секунд!')
+                            time.sleep(180)
+                    except Exception as ex:
+                        if mistake_global < 2:
+                            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                            message = template.format(type(ex).__name__, ex.args)
+                            mistake_global = mistake_global + 1
+                            print('У аккаунта ' + loginAndPass[0] + ' ГЛОБАЛЬНАЯ ошибка! ' + message)
+                            print('Засыпаю на 120 секунд')
+                            time.sleep(120)
+                            continue
+                        else:
+                            mistake_global = 0
+                            os.mkdir("from_global_mistake")
+                            getAllStatistic(accounts_with_mistake_pass, accounts_without_drop, accounts_with_empty_inventory, accounts_with_drop, folder='from_global_mistake/')
                             current_account_num = current_account_num + 1
-                            accounts_with_empty_inventory.append(loginAndPass[0] + ':' + loginAndPass[1])
-                            print('У аккаунта ' + loginAndPass[0] + ' не создан инвентарь в Вашей игре!')
+                            with open('from_global_mistake/MISTAKE.txt', "w") as file:
+                                file.writelines('Глобальная ошибка возникла на этом аккаунте: ' + loginAndPass[0] + ':' + loginAndPass[1] + '\n')
+                            print('Пропускаю аккаунт ' + loginAndPass[0] + ' потому что с ним что-то не так!')
                             break
-                    else:
-                        print('Ошибка ', response.status_code + '. Засыпаю на 180 секунд!')
-                        time.sleep(180)
                 time.sleep(25)
 
-        if len(accounts_without_drop) != 0:
-            with open('accounts_without_drop.txt', "w") as file:
-                file.writelines("%s\n" % line for line in accounts_without_drop)
+        os.mkdir("success_checking")
+        getAllStatistic(accounts_with_mistake_pass, accounts_without_drop, accounts_with_empty_inventory, accounts_with_drop, folder='success_checking/')
 
-        if len(accounts_with_empty_inventory) != 0:
-            with open('accounts_with_empty_inventory.txt', "w") as file:
-                file.writelines("%s\n" % line for line in accounts_with_empty_inventory)
+        if dataJSON['send_mail'] == 1:
+            smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
+            smtpObj.starttls()
+            smtpObj.login(dataJSON['info_about_mail']['login'], dataJSON['info_about_mail']['pass'])
 
-        with open('Accounts_with_drop.txt', "w") as file:
-            file.writelines("%s\n" % line for line in accounts_with_drop)
-
-        with open('Statistic.txt', "w") as file:
-            file.writelines('Общее кол-во кейсов: ' + sumCases(items_for_search) + '\n\n')
-            file.writelines('Кол-во кейсов каждого вида:\n\n')
+            message_info = 'Общее кол-во кейсов: ' + sumCases(items_for_search) + '\n\nКол-во кейсов каждого вида:\n\n'
+            massage_about_cases = ''
             for item in items_for_search:
-                file.writelines(item['name_item'] + ': ' + str(item['quantity']) + '\n')
+                massage_about_cases = massage_about_cases + item['name_item'] + ': ' + str(item['quantity']) + '\n'
 
-        print('Программа завершила свою работу!')
+            full_massage = message_info + massage_about_cases
+
+            smtpObj.sendmail(dataJSON['info_about_mail']['login'], dataJSON['info_about_mail']['host_mail'], full_massage.encode(encoding='utf-8'))
+            smtpObj.quit()
+            print('Письмо отправлено на Вашу почту^_^')
+
+        time_finish = str(datetime.datetime.today().strftime(f"%Y-%m-%d %H:%M:%S"))
+        print('Программа завершила свою работу '  + str(time_finish) + '!')
 
 
-    with open('config.json') as json_file:
-        dataJSON = json.load(json_file)
-        items_for_search = dataJSON['items_for_search']
 
-        if dataJSON['your_items'] == 'no':
-            if dataJSON['your_choose'] == 0:
-                start_script('CS:GO', items_for_search_CSGO)
-            elif dataJSON['your_choose'] == 1:
-                start_script('Dota 2', items_for_search_DOTA)
-            elif dataJSON['your_choose'] == 2:
-                print('Данная опция реализованна только с Вашими личными предметами, поэтому заколните эти предметы в файле config.json^_^')
-        else:
-            if dataJSON['your_choose'] == 0:
-                start_script('CS:GO', items_for_search)
-            elif dataJSON['your_choose'] == 1:
-                start_script('Dota 2', items_for_search)
-            elif dataJSON['your_choose'] == 2:
-                start_script('TF2', items_for_search)
+    if dataJSON['your_items'] == 'no':
+        if dataJSON['your_choose'] == 0:
+            start_script('CS:GO', items_for_search_CSGO)
+        elif dataJSON['your_choose'] == 1:
+            start_script('Dota 2', items_for_search_DOTA)
+        elif dataJSON['your_choose'] == 2:
+            print('Данная опция реализованна только с Вашими личными предметами, поэтому заполните эти предметы в файле config.json^_^')
+    else:
+        if dataJSON['your_choose'] == 0:
+            start_script('CS:GO', items_for_search)
+        elif dataJSON['your_choose'] == 1:
+            start_script('Dota 2', items_for_search)
+        elif dataJSON['your_choose'] == 2:
+            start_script('TF2', items_for_search)
 else:
     print('Неизвестная ошибка 404, обратитесь к создательнице программы!')
 input()
